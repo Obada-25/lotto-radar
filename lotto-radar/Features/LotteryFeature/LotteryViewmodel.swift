191b508eb943bb687832dec1be200f19db10d17a
Obada <obada95darkznly@gmail.com>
2025-09-15 17:23:22 +0200

    Created the view model

---
+++ lotto-radar/Features/LotteryFeature/LotteryViewmodel.swift
@@ -0,0 +1,40 @@
+//
+//  LotteryViewmodel.swift
+//  lotto-radar
+//
+//  Created by Obada Darkazanly on 15.09.25.
+//
+
+import Foundation
+
+final class LotteryViewmodel: ObservableObject {
+    // MARK: Properties
+    @Published var isLoading = false
+    @Published var error: Error?
+    @Published var lotteries: [LotteryResponse] = []
+    
+    private let networkingService: Networking
+    
+    // MARK: Initializer
+    init (networkingService: Networking) {
+        self.networkingService = networkingService
+    }
+    
+    // MARK: Methods
+    func fetchData() async {
+        isLoading = true
+        defer { isLoading = false }
+        
+        do {
+            let result = try await networkingService.fetchData()
+            switch result {
+            case .success(let lotteries):
+                self.lotteries = lotteries
+            case .failure(let error):
+                self.error = error
+            }
+        } catch {
+            self.error = error
+        }
+    }
+}
--- lotto-radar/lotto_radarApp.swift
+++ lotto-radar/lotto_radarApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct lotto_radarApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            LotteryView()
         }
     }
 }
