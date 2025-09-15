1a7d5e324aecfb03f16590c1f4d089b6c2fec6fe
Obada <obada95darkznly@gmail.com>
2025-09-15 16:52:11 +0200

    Added networking service and moved the models

---
+++ lotto-radar/Helpers/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  lotto-radar
+//
+//  Created by Obada Darkazanly on 15.09.25.
+//
+
+import Foundation
+
+enum Constants {
+    static let baseURL = "https://www.lotto24.de/public/v1/drawinfo/aggregated/6aus49,eurojackpot"
+}
---
+++ lotto-radar/Services/NetworkService.swift
@@ -0,0 +1,31 @@
+//
+//  BackendService.swift
+//  lotto-radar
+//
+//  Created by Obada Darkazanly on 15.09.25.
+//
+
+import Foundation
+
+protocol Networking {
+    func fetchData() async throws -> Result<LotteryResponse, Error>
+}
+
+actor NetworkingService: Networking {
+    func fetchData() async throws -> Result<LotteryResponse, any Error> {
+        guard let url = URL(string: Constants.baseURL) else {
+            throw NSError(domain: "Invalid URL", code: 0, userInfo: nil)
+        }
+        
+        let request = URLRequest(url: url)
+        
+        let (data, _) = try await URLSession.shared.data(for: request)
+        
+        do {
+            let decodedData = try JSONDecoder().decode(LotteryResponse.self, from: data)
+            return .success(decodedData)
+        } catch {
+            throw error
+        }
+    }
+}
